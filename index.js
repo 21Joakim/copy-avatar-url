3ad5676e7178238061c555dabf17ddd391d9e164
Joakim <21Joakim@users.noreply.github.com>
2019-11-14 17:48:56 +0100

    Misread the error

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ module.exports = class CopyAvatarUrl extends Plugin {
     inject('jockie-copyAvatarUrl-user', UserContextMenu.prototype, 'render', function (args, res) {
       const { children } = res.props.children.props.children.props;
 
-      const developerGroupIndex = children.findIndex(item => item.type && item.type.displayName === 'DeveloperModeGroup');
+      const developerGroupIndex = children.findIndex(item => item && item.type.displayName === 'DeveloperModeGroup');
 
       /* Backup in case it changes name */
       const foundComponent = developerGroupIndex !== -1;
