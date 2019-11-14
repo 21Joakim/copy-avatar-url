b5f896153e46fc9bf36bd528a9ee580883e41a4a
Joakim <21Joakim@users.noreply.github.com>
2019-11-14 12:19:01 +0100

    Files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Copy Avatar Url Plugin
+This is a plugin made for [Powercord](https://github.com/powercord-org/powercord) which makes it possible to quickly copy a user's avatar url.
+
+## Installation
+**git clone https://github.com/21Joakim/copy-avatar-url.git jockie-copyAvatarUrl**
+
+## Example
+![](Example.gif)
---
+++ index.js
@@ -0,0 +1,52 @@
+const { Plugin } = require('powercord/entities');
+const { getModuleByDisplayName, getModule, React } = require('powercord/webpack');
+const { ContextMenu: { Button } } = require('powercord/components');
+const { inject, uninject } = require('powercord/injector');
+
+const { clipboard } = require('electron');
+
+module.exports = class CopyAvatarUrl extends Plugin {
+  async import (filter, name = filter, useModule = false) {
+    if (typeof filter === 'string') {
+      filter = [ filter ];
+    }
+
+    if (useModule) {
+      this[name] = await getModule(filter);
+    } else {
+      this[name] = (await getModule(filter))[name];
+    }
+  }
+
+  async doImport () {
+    await this.import('developerMode', 'settings', true);
+  }
+
+  async startPlugin () {
+    await this.doImport();
+
+    const _this = this;
+
+    const UserContextMenu = await getModuleByDisplayName('UserContextMenu');
+    inject('jockie-copyAvatarUrl-user', UserContextMenu.prototype, 'render', function (args, res) { // eslint-disable-line func-names
+      const { children } = res.props.children.props.children.props;
+
+      const developerGroupIndex = children.findIndex(item => item.type.displayName === 'DeveloperModeGroupa');
+
+      /* Backup in case it changes name */
+      const foundComponent = developerGroupIndex !== -1;
+
+      children.splice((foundComponent ? developerGroupIndex + 1 : children.length), 0, React.createElement(Button, {
+        name: 'Copy Avatar Url',
+        onClick: () => clipboard.writeText(this.props.user.avatarURL),
+        seperate: !foundComponent || !_this.settings.developerMode
+      }));
+
+      return res;
+    });
+  }
+
+  pluginWillUnload () {
+    uninject('jockie-copyAvatarUrl-user');
+  }
+};
